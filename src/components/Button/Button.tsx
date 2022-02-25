a822331a13b61d251b72b452b6c9b5f98df30d32
Nilphym <nilphym@gmail.com>
2022-02-25 22:12:30 +0100

    Add missing props to button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,13 +4,27 @@ import { CollisionButton } from './CollisionButton';
 
 type ButtonProps = {
   children: React.ReactNode;
+  onClick: () => void;
+  type?: 'button' | 'submit' | 'reset';
   startIcon?: React.ReactNode;
   endIcon?: React.ReactNode;
 };
 
-export const Button = ({ children, startIcon, endIcon }: ButtonProps) => {
+export const Button = ({
+  children,
+  onClick,
+  type = 'button',
+  startIcon,
+  endIcon
+}: ButtonProps) => {
   return (
-    <CollisionButton variant="outlined" startIcon={startIcon} endIcon={endIcon}>
+    <CollisionButton
+      type={type}
+      onClick={onClick}
+      variant="outlined"
+      startIcon={startIcon}
+      endIcon={endIcon}
+    >
       {children}
     </CollisionButton>
   );

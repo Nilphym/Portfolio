c6454b8c4a2a9026fb81bc570ecff6b4dfe30ad5
Nilphym <nilphym@gmail.com>
2022-02-25 16:49:12 +0100

    Add text file for home page

---
+++ src/assets/text/homePage.ts
@@ -0,0 +1,4 @@
+export const headingText = ["Hey, I'm", 'Jędrzej,', 'front-end web developer.'];
+export const description =
+  'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis minima modi consequuntur rerum commodi aperiam. Expedita quo officiis molestias nostrum.';
+export const buttonText = 'Projects';

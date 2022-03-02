162d5fea79ed7bc64be8da44fb2273fb219fbd80
Nilphym <nilphym@gmail.com>
2022-03-02 23:08:04 +0100

    Export components

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,7 @@ export { PageHeading } from './PageHeading';
 export { NavMobile } from './NavMobile';
 export { Link } from './Link';
 export { NavDesktop, NavDesktopSocials } from './NavDesktop';
+export { FeaturedSkillCard } from './FeaturedSkillCard';
+export { Timeline } from './Timeline';
+export type { TimelineProps } from './Timeline';
+export { PersonalInfo } from './PersonalInfo';

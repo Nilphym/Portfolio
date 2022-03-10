61f014fc0bd8c0f15e287cc64947936e0d06fa09
Nilphym <nilphym@gmail.com>
2022-03-10 09:29:41 +0100

    Add contact page to routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import { HomePage, AboutPage } from './pages';
+import { HomePage, AboutPage, ContactPage } from './pages';
 import { Layout } from './Layout';
 
 export const App = () => {
@@ -10,6 +10,7 @@ export const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
         <Route path="about" element={<AboutPage />} />
+        <Route path="contact" element={<ContactPage />} />
         <Route path="*" element={<HomePage />} />
       </Route>
     </Routes>
